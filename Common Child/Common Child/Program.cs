deff67b1d2b36d6574913ad663899807a3d3eb73
Azael Pérez <62385359+azaelrock0@users.noreply.github.com>
2024-04-11 19:32:50 -0600

    Add files via upload

---
+++ Common Child/Common Child/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'commonChild' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int commonChild(string s1, string s2)
+    {
+        int[,] matrix = new int[s1.Length + 1, s2.Length + 1];
+
+        for (int i = 0; i < s1.Length + 1; i++)
+        {
+            matrix[0, i] = 0;
+            matrix[i, 0] = 0;
+        }
+
+        for (int i = 1; i < s1.Length + 1; i++)
+        {
+            for (int j = 1; j < s2.Length + 1; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                    matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                else if (matrix[i - 1, j] > matrix[i, j - 1])
+                    matrix[i, j] = matrix[i - 1, j];
+                else
+                    matrix[i, j] = matrix[i, j - 1];
+            }
+        }
+
+        return matrix[s1.Length, s2.Length];
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s1 = Console.ReadLine();
+
+        string s2 = Console.ReadLine();
+
+        int result = Result.commonChild(s1, s2);
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
---
+++ Reverse Shuffle Merge/Reverse Shuffle Merge/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Runtime.CompilerServices;
+
+class Result
+{
+
+    /*
+     * Complete the 'reverseShuffleMerge' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string reverseShuffleMerge(string s)
+    {
+        SortedDictionary<char, int> unused = new SortedDictionary<char, int>();
+        SortedDictionary<char, int> used = new SortedDictionary<char, int>();
+        SortedDictionary<char, int> required = new SortedDictionary<char, int>();
+
+        foreach (char c in s)
+            if (unused.ContainsKey(c))
+                unused[c]++;
+            else
+                unused.Add(c, 1);
+
+        foreach (KeyValuePair<char, int> kvp in unused)
+            used.Add(kvp.Key, 0);
+
+        foreach (KeyValuePair<char, int> kvp in unused)
+            required.Add(kvp.Key, kvp.Value / 2);
+
+        char[] reverseArray = s.ToCharArray();
+        Array.Reverse(reverseArray);
+        string reverse = new string(reverseArray);
+
+        List<char> res = new List<char>();
+
+        foreach (char c in reverse)
+        {
+            unused[c]--;
+            if (used[c] < required[c])
+            {
+                res.Add(c);
+                used[c]++;
+                while (res.Count > 1 && res[res.Count - 1] < res[res.Count - 2])
+                {
+                    char temp = res[res.Count - 2];
+                    if (unused[temp] + used[temp] - 1 >= required[temp])
+                    {
+                        res.RemoveAt(res.Count - 2);
+                        used[temp]--;
+                    }
+                    else
+                        break;
+                }
+            }
+        }
+
+        string resString = new string(res.ToArray());
+        return resString;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        string result = Result.reverseShuffleMerge(s);
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
